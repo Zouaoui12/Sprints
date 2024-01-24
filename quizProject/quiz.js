4853969269b5a1a8c89dfac7a64f4764e775cd2b
Mestiri77 <faroukmestiri77@gmail.com>
2024-01-24 09:31:14 +0100

    quizProject

---
+++ quizProject/quiz.js
@@ -0,0 +1,23 @@
+const quizQuestions = [
+    {
+      description:
+        "A ninja with a dream of becoming the strongest ninja and leader of his village.",
+      answer: "Naruto",
+      point: 5,
+    },
+    {
+      description:
+        "A high school student who gains the powers of a death god and uses them to bring justice.",
+      answer: "Death Note",
+      point: 10,
+    },
+    {
+      description:
+        "A group of friends embark on an adventure to find a mysterious treasure.",
+      answer: "One Piece",
+      point: 2,
+    },
+  ];
+  
+  
+  
