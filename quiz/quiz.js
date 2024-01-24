4937be25116055e0be0d94355197c1fedd4ac88d
Mestiri77 <faroukmestiri77@gmail.com>
2024-01-24 10:55:32 +0100

    quiz project

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -4,20 +4,25 @@ const quizQuestions = [
         "A ninja with a dream of becoming the strongest ninja and leader of his village.",
       answer: "Naruto",
       point: 5,
+      coverImg : "https://i.pinimg.com/564x/85/31/7b/85317b621555591cfdc1daebd40f399d.jpg",
+      character : "https://photos.tf1.fr/780/780/perso-showpage-8efac4-fe1481-0@1x.png"
     },
     {
       description:
         "A high school student who gains the powers of a death god and uses them to bring justice.",
       answer: "Death Note",
       point: 10,
+      coverImg : "https://m.media-amazon.com/images/M/MV5BNjRiNmNjMmMtN2U2Yi00ODgxLTk3OTMtMmI1MTI1NjYyZTEzXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
+      character : "/assets/img/death.png"
     },
     {
       description:
         "A group of friends embark on an adventure to find a mysterious treasure.",
       answer: "One Piece",
       point: 2,
+      coverImg:"https://www.manga-news.com/public/images/pix/dvd/690/One-piece-anime-east-blue.jpg",
+      character : "/assets/img/onePiece.png"
     },
   ];
-  
-  
-  
+ 
+ 
